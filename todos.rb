5d3b7c8a75214272492a190e1603da7af6bc590c
Teddy <pazteddy@gmail.com>
2022-05-12 11:25:17 -0400

    Adicionado el welcome y la lista de tareas

--- todos.rb
+++ todos.rb
@@ -1,1 +1,18 @@
-# Happy coding 😎
+todos = [
+  { "id" => 1, "content" => "Fill the weekly feedback", "completed" => false },
+  { "id" => 2, "content" => "Complete Ruby Basics 1", "completed" => false },
+  { "id" => 3, "content" => "Complete Ruby Basics 2", "completed" => false },
+  { "id" => 4, "content" => "Complete Ruby Methods", "completed" => false },
+  { "id" => 5, "content" => "Do meditation", "completed" => false }
+]
+
+def list_todos(todos)
+  puts "------------------------Welcome to toDOS------------------------"
+  todos.each do |todo|
+    puts "#{todo['id']}. #{todo['content']}"
+  end
+end
+
+# Main program
+
+list_todos(todos)
